5d720e170c4a3e1461281823c0df03cdbbe2386c
wns-nehash <133336683+wns-nehash@users.noreply.github.com>
2024-12-16 13:00:17 +0530

    Adding model_building and model_eval.py

---
+++ src/model/model_building.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+import os
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+import yaml
+# n_estimator = yaml.safe_load(open("params.yaml", "r"))["model_building"]["n_estimator"]
+
+def load_params(filepath:str)->int:
+    try:
+        with open(filepath,'r') as file:
+            params = yaml.safe_load(file)
+        return params["model_building"]["n_estimator"]
+    except Exception as e:
+        raise Exception(f"Error loading paramas {filepath}:{e}")
+
+def load_data(filepath:str) -> pd.DataFrame:
+    try:
+        return pd.read_csv(filepath)
+    except Exception as e:
+        raise Exception(f"Error occured loading data{filepath}:{e}")
+    
+def prepare_data(data: pd.DataFrame) -> tuple[pd.DataFrame,pd.Series]:
+    try:
+        X_train = data.drop(columns=['Potability'],axis=1)
+        y_train = data['Potability']
+        return X_train,y_train
+    except Exception as e:
+        raise Exception(f"Error occured preparing data:{e}")
+
+#train_data = pd.read_csv("./data/processed/train_processed.csv")
+# X_train = train_data.iloc[:,0:-1].values
+# y_train =train_data.iloc[:,-1].values
+
+# X_train = train_data.drop(columns=['Potability'],axis=1)
+# y_train =train_data['Potability']
+
+# clf = RandomForestClassifier(n_estimators=n_estimator)
+# clf.fit(X_train, y_train)
+
+def train_model(X_train:pd.DataFrame, y_train:pd.Series, n_estimator:int)-> RandomForestClassifier:
+    try:
+        clf = RandomForestClassifier(n_estimators=n_estimator)
+        model = clf.fit(X_train,y_train)
+        return model
+    except Exception as e:
+        raise Exception(f"Error training model:{e}")
+
+def save_model (model: RandomForestClassifier, filepath:str) ->None:
+    try:
+        pickle.dump(model,open("model.pkl","wb"))
+    except Exception as e:
+        raise Exception(f"Error saving model:{e}")
+
+def main():
+    try:
+        processed_data_path = r"./data/processed/train_processed.csv"
+        params_path ="./params.yaml"
+        model_name = "model.pkl"
+        n_estimator =load_params(params_path)
+        processed_data = load_data(processed_data_path)
+        train_processed_data, test_processed_data =prepare_data(processed_data)
+        model = train_model(train_processed_data, test_processed_data, n_estimator)
+        save_model(model,model_name)
+ 
+    except Exception as e:
+        raise Exception("An error occured: {e}")
+
+
+
+
+if __name__=="__main__":
+    main()
+
+
+
---
+++ src/model/model_eval.py
@@ -0,0 +1,82 @@
+import numpy as np
+import pandas as pd
+import pickle
+import json
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score,precision_score, recall_score, f1_score
+
+def load_data(filepath:str) -> pd.DataFrame:
+    try:
+        return pd.read_csv(filepath)
+    except Exception as e:
+        raise Exception(f"Error Loading data from {filepath}:{e}")
+
+# test_data = pd.read_csv("./data/processed/test_processed.csv")
+
+def prepare_data(data:pd.DataFrame) -> tuple[pd.DataFrame, pd.Series]:
+    try:
+        X_test = data.drop(columns=['Potability'],axis=1)
+        y_test = data['Potability']
+        return X_test,y_test
+    except Exception as e:
+        raise Exception(f"Error preparing data:{e}")
+    
+def load_model(filepath:str):
+    try:
+        with open("model.pkl","rb") as file:
+            model = pickle.load(file)
+        return model
+    except Exception as e:
+        raise Exception(f"Error loading model:{e}")
+
+# model = pickle.load(open("model.pkl", "rb"))
+# y_pred = model.predict(X_test)
+
+
+
+def eval_score(model,X_test:pd.Series, y_test:pd.Series) -> dict:
+    try:
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test,y_pred)
+        precision = precision_score(y_test,y_pred)
+        recall = recall_score(y_test,y_pred)
+        f1score =f1_score(y_test,y_pred)
+
+        metrics_dict ={
+            "accuracy":accuracy,
+            "precision":precision,
+            "recall":recall,
+            "f1score":f1score
+        }
+        return metrics_dict
+    except Exception as e:
+        raise Exception(f"Error making prediction:{e}")
+
+
+def save_metrics(metrics:dict, filepath:str) ->None:
+    try:
+        with open(filepath,"w") as file:
+            json.dump(metrics,file,indent=4)
+    except Exception as e:
+        raise Exception(f"Error saving metrics:{e}")
+    
+
+def main():
+    try:
+        metrics_filepath ="metrics.json"
+        model_path ="model.pkl"
+        processed_testdata_path =r"./data/processed/test_processed.csv"
+
+        testdata_path = load_data(processed_testdata_path)
+        X_test, y_test =prepare_data(testdata_path)
+        model = load_model(model_path)
+
+        metrics_dict = eval_score(model,X_test, y_test)
+        save_metrics(metrics_dict,metrics_filepath)
+        
+    except Exception as e:
+        raise Exception(f"An error occured :{e}")
+
+
+if __name__=="__main__":
+    main()
